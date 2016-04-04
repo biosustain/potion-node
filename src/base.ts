2e94d0bc3987a71b069295370ee26ba7caa7dcb0
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-04-04 13:13:25 +0200

    refactor(Potion): make cache private

--- src/base.ts
+++ src/base.ts
@@ -125,10 +125,10 @@ export interface PotionRequestOptions {
 
 export abstract class PotionBase {
 	resources = {};
-	cache: PotionItemCache<Item>;
 	promise = Promise;
 
 	private _prefix: string;
+	private _cache: PotionItemCache<Item>;
 	private _promises = [];
 
 	static create() {
@@ -137,7 +137,7 @@ export abstract class PotionBase {
 
 	constructor({prefix = '', cache}: PotionOptions = {}) {
 		this._prefix = prefix;
-		this.cache = cache;
+		this._cache = cache;
 	}
 
 	parseURI(uri: string) {
@@ -169,8 +169,8 @@ export abstract class PotionBase {
 
 	get(uri, options?: PotionRequestOptions): Promise<any> {
 		// Try to get from cache
-		if (this.cache && this.cache.get) {
-			const instance = this.cache.get(uri);
+		if (this._cache && this._cache.get) {
+			const instance = this._cache.get(uri);
 			if (instance) {
 				return Promise.resolve(instance);
 			}
@@ -208,8 +208,8 @@ export abstract class PotionBase {
 
 		return this.request(uri, {method: 'DELETE'}).then(() => {
 			// Clear the item from cache if exists
-			if (this.cache && this.cache.get && this.cache.get(uri)) {
-				this.cache.clear(uri);
+			if (this._cache && this._cache.get && this._cache.get(uri)) {
+				this._cache.clear(uri);
 			}
 		});
 	}
@@ -260,8 +260,8 @@ export abstract class PotionBase {
 
 					// TODO: might make sense to move this logic somewhere else
 					let instance = Reflect.construct(<any>resource, [obj]);
-					if (this.cache && this.cache.put) {
-						this.cache.put(uri, <any>instance);
+					if (this._cache && this._cache.put) {
+						this._cache.put(uri, <any>instance);
 					}
 
 					return instance;

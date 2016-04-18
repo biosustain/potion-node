de698f50932d47c30e46fd8258eda2dc4624bae3
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-04-18 14:05:11 +0200

    chore(potion/fetch): properly use the Request class

--- src/fetch.ts
+++ src/fetch.ts
@@ -18,34 +18,31 @@ export class Potion extends PotionBase {
 		super(Object.assign({cache: new MemCache()}, options));
 	}
 
+	// Use window.fetch for making requests,
+	// see https://developer.mozilla.org/en-US/docs/Web/API/GlobalFetch/fetch for API.
+	// Polyfill at https://github.com/github/fetch.
+	// let {method, data, cache} = Object.assign({method: 'GET', cache: true}, options);
 	request(uri, {method = 'GET', data = null, cache = true}: PotionRequestOptions = {}): Promise<any> {
-		// Use window.fetch for making requests,
-		// see https://developer.mozilla.org/en-US/docs/Web/API/GlobalFetch/fetch for API.
-		// Polyfill at https://github.com/github/fetch.
-		// let {method, data, cache} = Object.assign({method: 'GET', cache: true}, options);
-		let init: RequestInit = {
+		let headers: Headers = new Headers();
+		let init: any = {
 			method,
-			cache: cache ? 'default' : 'no-cache'
+			cache: cache ? 'default' : 'no-cache',
+			// Make sure cookies are sent
+			// https://github.com/github/fetch#sending-cookies
+			credentials: 'include'
 		};
-
-		// Make sure cookies are sent
-		// https://github.com/github/fetch#sending-cookies
-		init.credentials = 'include';
 
 		if (data) {
 			// POST/PUT/PATCH needs headers and JSON body,
 			// see https://github.com/github/fetch#post-json for more info.
-			let headers: Headers = new Headers();
 			headers.set('Accept', 'application/json');
 			headers.set('Content-Type', 'application/json');
-
-			Object.assign(init, {
-				headers,
-				body: JSON.stringify(data)
-			});
+			init.body = JSON.stringify(data);
 		}
 
-		return fetch(uri, init).then((response) => {
+		Object.assign(init, {headers});
+
+		return fetch(new Request(uri, init), init).then((response) => {
 			if (response.ok) {
 				return response.json().then(
 					(json) => {

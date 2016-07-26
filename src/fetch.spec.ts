ff327d3f796a1691be90b3ae453ccf42ed0425b1
rolandjitsu <rolandjitsu@gmail.com>
2016-07-26 13:33:17 +0200

    chore: update fetch-mock

--- src/fetch.spec.ts
+++ src/fetch.spec.ts
@@ -1,11 +1,8 @@
 // Mock request responses using
 // https://www.npmjs.com/package/fetch-mock
-import * as fetchMock from 'fetch-mock';
+const fetchMock = require('fetch-mock');
 
-import {
-	Potion,
-	Item
-} from './fetch';
+import {Potion,	Item} from './fetch';
 
 
 describe('potion/fetch', () => {
@@ -22,19 +19,19 @@ describe('potion/fetch', () => {
 			});
 
 			it('should make a XHR request', () => {
-				fetchMock.mock('http://localhost/ping', 'GET', {});
+				(fetchMock as any).get('http://localhost/ping', {});
 				potion.fetch('http://localhost/ping');
 				expect(fetchMock.called('http://localhost/ping')).toBe(true);
 			});
 
 			it('should use \'no-cache\' for the window.fetch() request {cache} option if {cache} option is set to false', () => {
-				fetchMock.mock('http://localhost/ping', 'GET', {});
+				(fetchMock as any).get('http://localhost/ping', {});
 				potion.fetch('http://localhost/ping', {cache: false});
 				expect((fetchMock.lastOptions('http://localhost/ping') as any).cache).toEqual('no-cache');
 			});
 
 			it('should use the appropriate request method set by the {method} option', () => {
-				fetchMock.mock('http://localhost/ping', 'PATCH', {});
+				fetchMock.mock('http://localhost/ping', {}, {method: 'PATCH'});
 				potion.fetch('http://localhost/ping', {method: 'PATCH'});
 				expect(fetchMock.called('http://localhost/ping')).toBe(true);
 			});
@@ -42,7 +39,7 @@ describe('potion/fetch', () => {
 			it('should pass anything set on {data} option as the {body} property of the request in JSON format', () => {
 				let body = null;
 				let headers: Headers = null;
-				fetchMock.mock('http://localhost/ping', 'POST', (url, opts: any) => {
+				(fetchMock as any).post('http://localhost/ping', (url, opts: any) => {
 					headers = opts.headers;
 					body = opts.body;
 					return 200;
@@ -61,18 +58,18 @@ describe('potion/fetch', () => {
 			});
 
 			it('should pass on the query params from the {search} option', () => {
-				fetchMock.mock('http://localhost/ping?pong=true&count=1', 'GET', 200);
+				(fetchMock as any).get('http://localhost/ping?pong=true&count=1', 200);
 				potion.fetch('http://localhost/ping', {method: 'GET', search: {pong: true, count: 1}});
 				expect(fetchMock.called('http://localhost/ping?pong=true&count=1')).toBe(true);
 			});
 
 			it('should return a Promise', () => {
-				fetchMock.mock('http://localhost/ping', 'GET', {});
+				(fetchMock as any).get('http://localhost/ping', {});
 				expect(potion.fetch('http://localhost/ping') instanceof Promise).toBe(true);
 			});
 
 			it('should return a Promise with data', (done) => {
-				fetchMock.mock('http://localhost/ping', 'GET', {body: {pong: true}});
+				(fetchMock as any).get('http://localhost/ping', {body: {pong: true}});
 				potion.fetch('http://localhost/ping').then((data) => {
 					expect(data).not.toBeUndefined();
 					expect(data).toEqual({pong: true});
@@ -90,7 +87,7 @@ describe('potion/fetch', () => {
 				@potion.registerAs('/user')
 				class User extends Item {}
 
-				fetchMock.mock('http://localhost/user/1', 'GET', {$uri: '/user/1'});
+				(fetchMock as any).get('http://localhost/user/1', {$uri: '/user/1'});
 
 				User.fetch(1).then(() => {
 					expect(User.store.cache.get('/user/1')).not.toBeUndefined();

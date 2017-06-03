87308776de9068b8846f5c95a38d449860566c6e
rolandjitsu <rolandjitsu@gmail.com>
2017-06-03 17:18:22 +0200

    chore: move Rollup config to /config

--- scripts/build_angularjs.sh
+++ scripts/build_angularjs.sh
@@ -11,7 +11,7 @@ DIST_FILENAME="${DIST_DIR}/${LIB_NAME}"
 # Build ES2015
 $(npm bin)/tsc -p src/tsconfig.angularjs.json --outDir ${ES2015_OUTPUT_DIR} --target ES2015
 # Rollup ES2015 FESM
-$(npm bin)/rollup -c src/rollup.config.js\
+$(npm bin)/rollup -c config/rollup.config.js\
     -i "${ES2015_OUTPUT_DIR}/${LIB_NAME}.js"\
     -o "${DIST_FILENAME}.js"\
     --format es
@@ -22,18 +22,18 @@ cp "${ES2015_OUTPUT_DIR}/${LIB_NAME}.d.ts" ${DIST_DIR}
 # Build ES5
 $(npm bin)/tsc -p src/tsconfig.angularjs.json
 # Rollup ES5 FESM, UMD
-$(npm bin)/rollup -c src/rollup.config.js\
+$(npm bin)/rollup -c config/rollup.config.js\
     -i "${ES5_OUTFILE}"\
     -o "${DIST_FILENAME}.umd.min.js"\
     --format umd\
     --environment UGLIFY
 
-$(npm bin)/rollup -c src/rollup.config.js\
+$(npm bin)/rollup -c config/rollup.config.js\
     -i "${ES5_OUTFILE}"\
     -o "${DIST_FILENAME}.umd.js"\
     --format umd
 
-$(npm bin)/rollup -c src/rollup.config.js\
+$(npm bin)/rollup -c config/rollup.config.js\
     -i "${ES5_OUTFILE}"\
     -o "${DIST_FILENAME}.es5.js"\
     --format es
